72718696ec5442ed0841c26cef136422d22dea6d
charan149 <charanram149@gmail.com>
2020-05-01 18:02:06 +0530

    last of project will end

---
+++ src/test/java/org/git/Day1.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class Day1 {
+	public void cliId() {
+		System.out.println("cli ID is 1234");
+	}
+	public void met() {
+		System.out.println("chharan");
+	}
+
+	public static void main(String[] args) {
+		Day1 ss=new Day1();
+		ss.cliId();
+		ss.met();
+		
+	}
+}
